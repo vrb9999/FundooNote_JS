6fbeb9ee42f014cd6303f619e6ff994f88ac9d6a
vrb9999 <vbiradar9999@gmail.com>
2022-08-23 02:04:58 +0530

    Menu toggle and create note design

---
+++ FundooNote_JS/script/note/dashboard.js
@@ -0,0 +1,57 @@
+var nav = false;
+function menutoggle(){
+    // let menu = document.querySelector("#menu");
+    // let sidenav = document.querySelector(".side-nav");
+
+    // sidenav.classList.toggle("active");
+    
+    nav ? closeNav() : openNav();
+}
+
+function openNav(){
+    document.getElementById("side-nav").style.width = "250px";
+    document.getElementById("display-area").style.marginLeft = "250px";
+    nav = true;
+}
+
+function closeNav(){
+    document.getElementById("side-nav").style.width = "85px";
+    document.getElementById("display-area").style.marginLeft = "85px";
+    nav = false;
+}
+
+var noteArray;
+
+
+function getAllNotes(){
+    let token = localStorage.getItem('token');
+
+    $.ajax({
+        url:'https://localhost:44346/Note/GetAllNote',
+        type:'GET',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+        success: function(result){
+            console.log(result.data);
+            noteArray = result.data;
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+document.getElementById('notes').addEventListener('click', (notes)=>{
+    console.log("Notes : ", notes.target);
+})
+
+function displayNotes(noteArray){
+    console.log("Notes list : ", noteArray);
+
+    // document.getElementById('display').innerHTML = notesList.map((notes)=>
+    // ``
+    // )
+
+}
--- FundooNote_JS/script/user/login.js
+++ FundooNote_JS/script/user/login.js
@@ -80,6 +80,8 @@ function login(){
         },
         success: function(result){
             console.log(result);
+            localStorage.setItem('token', result.data);
+            window.location.href='../note/dashboard.html';
         },
         error: function(error){
             console.log(error);
