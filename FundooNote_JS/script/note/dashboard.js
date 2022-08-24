b3d07fcbfea6e54500692e07e469fec85063728f
vrb9999 <vbiradar9999@gmail.com>
2022-08-24 10:53:20 +0530

    Create notes api call

--- FundooNote_JS/script/note/dashboard.js
+++ FundooNote_JS/script/note/dashboard.js
@@ -1,10 +1,5 @@
 var nav = false;
-function menutoggle(){
-    // let menu = document.querySelector("#menu");
-    // let sidenav = document.querySelector(".side-nav");
-
-    // sidenav.classList.toggle("active");
-    
+function menutoggle(){    
     nav ? closeNav() : openNav();
 }
 
@@ -20,7 +15,60 @@ function closeNav(){
     nav = false;
 }
 
+function openCreateNote(){
+    document.getElementById("form").style.height="165px";
+    document.getElementById("pin").style.visibility="visible";
+}
+
+function closeCreateNote(){
+    document.getElementById("form").style.height="55px";
+    document.getElementById("pin").style.visibility="hidden";
+}
+
+
+function createNote(){
+    document.getElementById('form').addEventListener('submit', function(c){
+        c.preventDefault();
+    })
+    let token = localStorage.getItem('token');
+
+    let title = document.getElementById("title");
+    let description = document.getElementById("description");
+    let bgColor = 'green';
+
+    let noteData = {
+        title:title.value,
+        description:description.value,
+        bgcolor:bgColor
+    }
+    console.log(noteData);
+
+    $.ajax({
+        url:'https://localhost:44346/Note/AddNote',
+        type:'POST',
+        data:JSON.stringify(noteData),
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " +token
+        },
+        success: function(result){
+            console.log(result);
+            resetCreateNote();
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
+function resetCreateNote(){
+    document.getElementById('title').value="";
+    document.getElementById('description').value="";
+}
+
+
 var noteArray;
+
 getAllNotes();
 function getAllNotes(){
     let token = localStorage.getItem('token');
@@ -35,6 +83,7 @@ function getAllNotes(){
         success: function(result){
             console.log(result.data);
             noteArray = result.data;
+            noteArray.reverse();
         },
         error: function(error){
             console.log(error);
@@ -42,15 +91,10 @@ function getAllNotes(){
     })
 }
 
-// document.getElementById('notes').addEventListener('click', (notes)=>{
-//     console.log("Note : ", notes.target);
-// })
-
-// function displayNotes(noteArray){
-//     console.log("Notes list : ", noteArray);
-
-    // document.getElementById('display').innerHTML = notesList.map((notes)=>
-    // ``
-    // )
-
-//}
+document.getElementById('notes').addEventListener('click', (notes)=>{
+    console.log("Notes : ",notes.target);
+    getArray = noteArray.filter((filterNote)=>{
+        return filterNote.isArchive==false && filterNote.isTrash==false;
+    })
+    console.log("Notes : ", getArray);
+})
